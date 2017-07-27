76a1a5ebaff3e2d3cf2b34e55290d2693d85f9b1
Dilou89 <odil_cadiou@orange.fr>
2017-07-27 12:02:32 +0200

    maj

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-    	$this->call(UsersTableSeeder::class);
+    	//$this->call(UsersTableSeeder::class);
+    	DB::table('users')->insert([
+    		'email' => 'test@test.com',
+    		'password'=>bcrypt('pass'),
+
+    		]);
     }
 }
