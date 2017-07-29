cb21aab1a76beb0130c48c5b13b03120767466dd
raphael <moudens.raphael@orange.fr>
2017-07-29 15:53:32 +0200

    page accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,97 +10,65 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" type="text/css" href="/style.css">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
     <!-- Styles -->
     <style>
-        html, body{
-            /*background-color: #fff;*/
-            /*background-color: #66ff99;*/
-            background-color: #ff3333;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            /*font-size: 84px;*/
-            font-size: 150px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
     </style>
 </head>
 <body>
-    <div class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @if (Auth::check())
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ url('/login') }}">Login</a>
-            @endif
+    <header>
+        <div class="containerBannier">
+            <div class="col-sm-12" >
+                <img src="/images/cropped-BannierSite-2.png" alt="">
+            </div>
         </div>
+    </header>   
+</div>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+    <div class="bottom-right links">
+        @if (Auth::check())
+        <a href="{{ url('/home') }}">Home</a>
+        @else
+        <a href="{{ url('/login') }}">Login</a>
         @endif
-
-        <div class="content">
-            <div class="title m-b-md">
-
-            </div>
-
-            <div class="links">
-                
-                
-                <a href="https://laravel.com/docs">Documentation</a>
-                <a href="https://laracasts.com">Laracasts</a>
-                <a href="https://laravel-news.com">News</a>
-                <a href="https://forge.laravel.com">Forge</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-                <a href="https://github.com/laravel/laravel">GitHub</a>
-                
-                
-            </div>
-
-            <div>
-                
-            </div>
-        </div>
     </div>
+    @endif
+
+    <div class="content">
+
+
+        <div class="title m-b-md">
+
+
+        </div>
+
+        <div class="links">
+            <div class="container-fluid">
+                <h1>Les pommes</h1>
+                <p>Vous vous apprêtez à lancer une recherche pour identifier une variété de pomme.<br> 
+
+                    Quelques informations s'imposent :<br>
+
+                    Nous avons fait le choix d'un outil facile à utiliser, avec peu de critères, eux-mêmes faciles à observer ; vous risquez donc d'aboutir non pas à une seule pomme, mais à plusieurs. Vous pourrez vous reporter au descriptif des pommes sélectionnées pour affiner votre recherche : la période de consommation par exemple, les caractéristiques gustatives ou les photos pourront vous aider à les départager.<br> 
+                    Prenez obligatoirement plusieurs fruits (3 au minimum, 5-6 si possible, provenant impérativement du même arbre) car ils ne sont pas tous représentatifs et peuvent vous conduire à une mauvaise identification. Une pomme qui a poussé au centre d'un arbre bien touffu n'aura pas la même coloration qu'une pomme située en périphérie par exemple ; les arbres voisins prétendûment de la même variété ne le sont pas toujours non plus..... <br> 
+                    La base de données va augmenter au fil du temps, vous ne trouverez pour l'instant que les pommes étudiées depuis longtemps et sur lesquelles nous avons accumulé renseignements et photos. Si votre recherche n'aboutit pas, vous aurez sans doute plus de chance dans quelques mois.</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                         <a href="">Identifier une pomme</a> 
+                     </div>
+                     <div class="col-md-6">
+                         <a href="">Nos variétés</a>
+                     </div>
+                 </div>
+             </div>
+         </div>
+
+         <div>
+
+         </div>
+     </div>
+ </div>
 </body>
 </html>
