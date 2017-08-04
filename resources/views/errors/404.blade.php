d84b2297cb8e6e69f26439505d918ff59564db68
raphael <moudens.raphael@orange.fr>
2017-08-04 12:24:02 +0200

    page 404 fini

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,19 +2,14 @@
 
 @section('content')
 <body>
-    <div class="container">
-       <img src="{{ asset('images/logo.png') }}" alt="" class="logo">
-       <section class="descriptif">
-           <h1>Nous sommes désolés<br> mais cette page n'existe pas</h1><br>
-       </section>
-   </div>
-   <img src="{{ asset('/images/ver_pomme.jpg') }}"  class="img-responsive center-block " alt=" image message erreur">
+ <div class="col-md-12 text-center">
+   <a href="/accueil" class="btn btn-primary" id="retouraccueil">Retour à l'accueil</a>
+</div>
+ 
+   <img src="{{ asset('/images/ver.jpg') }}"  class="img-responsive center-block " alt=" image message erreur">
 
 
 
-   <div class="col-md-12 text-center">
-       <a href="/accueil" class="btn btn-primary" id="retouraccueil">Retour à l'accueil</a>
-   </div>
 </div>
 <br>
 <br>
