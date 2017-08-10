003ca9eaf2f0be82148f0d74b53e890c57e90811
raphael <moudens.raphael@orange.fr>
2017-08-10 13:48:27 +0200

    ajout securité admin

--- routes/web.php
+++ routes/web.php
@@ -35,22 +35,27 @@ Route::post('/recherche', 'DeterminationController@index');
 Route::get('/resultat', 'DeterminationController@recherche');
 
 //Admin : 
-//creer lister modifier et supprimer une variete afficher les infos
-
-Route::get('/adminlist', 'ModificationController@index');
-Route::get('/creation', function(){
-	return view('creation');
-});
-Route::post('/add', 'ModificationController@add');
-Route::get('/show/{id}','ModificationController@show');
-Route::get('/edition/{id}', 'ModificationController@edit');
-// Route::post('/store', 'ModificationController@store')->name('storeApple');
-Route::post('/update/{id}', 'ModificationController@update')->name('update');
-Route::get('/delete/{id}', 'ModificationController@destroy');
-//Route gallery images
-Route::get('/image-gallery', 'ImageGalleryController@index');
-Route::get('/list-gallery', 'ImageGalleryController@list');
-Route::get('/variety-description', 'ImageGalleryController@description');
 Route::post('image-gallery', 'ImageGalleryController@upload');
 Route::delete('image-gallery/{id}', 'ImageGalleryController@destroy');
+
+Route::group(['middleware' => ['isadmin']], function () {
+//creer lister modifier et supprimer une variete afficher les infos
+
+	Route::post('/add', 'ModificationController@add');
+	Route::get('/show/{id}','ModificationController@show');
+	Route::get('/edition/{id}', 'ModificationController@edit');
+// Route::post('/store', 'ModificationController@store')->name('storeApple');
+	Route::post('/update/{id}', 'ModificationController@update')->name('update');
+	Route::get('/delete/{id}', 'ModificationController@destroy');
+//Route gallery images
+	Route::get('/image-gallery', 'ImageGalleryController@index');
+	Route::get('/list-gallery', 'ImageGalleryController@list');
+	Route::get('/variety-description', 'ImageGalleryController@description');
+
+	Route::get('/adminlist', 'ModificationController@index');
+	Route::get('/creation', function(){
+		return view('creation');
+	});
+	
+});
 
